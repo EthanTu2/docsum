01d594a5cde889c7936d7e5d5e0e5b4b62951fd8
Ethan Tu <ethantu@Ethans-MacBook-Pro-8.local>
2024-09-05 10:16:28 -0700

    docsum √

--- docsum.py
+++ docsum.py
@@ -1,17 +1,34 @@
 import os
 from groq import Groq
 
+# parse command line args
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('filename')
+args = parser.parse_args()
+
+# line 7+8 => args.filename will contain the first string after program name on command line
+
+#print(args) #testing update: works now!
+
 client = Groq(
     # This is the default and can be omitted
     api_key=os.environ.get("GROQ_API_KEY"),
 )
 
+with open(args.filename) as f:
+    text = f.read()
+
 chat_completion = client.chat.completions.create(
     messages=[
         {
+            "role": 'system',
+            'content': 'Summarize the input text below. Limit the summary to 1 paragraph and use a 1st grade reading level.',
+        },
+        {
             "role": "user",
-            "content": "Explain the importance of low latency LLMs",
-        }
+            "content": text,
+            }
     ],
     model="llama3-8b-8192",
 )
